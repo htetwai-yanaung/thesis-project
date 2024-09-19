b92992c8a94bc71dc7c4da3763da12e16e2c6621
htetwaiyanaung <htetwaiyanaung.sgg@gmail.com>
2024-09-20 01:30:32 +0630

    admin dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,35 +15,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(30)->create();
+        \App\Models\User::factory(30)->create();
 
-        // \Modules\Core\App\Models\News::factory(10)->create();
-        // \Modules\Core\App\Models\ThesisProject::factory(10)->create();
+        \Modules\Core\App\Models\News::factory(10)->create();
+        \Modules\Core\App\Models\ThesisProject::factory(10)->create();
 
-        // for($i = 1; $i <= 10; $i++){
-        //     \Modules\Core\App\Models\Image::factory()->create(['parent_id' => $i, 'image_type' => 'news']);
-        // }
-        // for($i = 1; $i <= 10; $i++){
-        //     \Modules\Core\App\Models\Image::factory()->create(['parent_id' => $i, 'image_type' => 'project']);
-        // }
+        for($i = 1; $i <= 10; $i++){
+            \Modules\Core\App\Models\Image::factory()->create(['parent_id' => $i, 'image_type' => 'news']);
+        }
+        for($i = 1; $i <= 10; $i++){
+            \Modules\Core\App\Models\Image::factory()->create(['parent_id' => $i, 'image_type' => 'project']);
+        }
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
 
-        // $roles = ['Admin', 'Teacher', 'Student'];
-        // foreach($roles as $role){
-        //     UserRole::create([
-        //         'role' => $role,
-        //     ]);
-        // }
+        $roles = ['Admin', 'Teacher', 'Student'];
+        foreach($roles as $role){
+            UserRole::create([
+                'role' => $role,
+            ]);
+        }
 
-        // $years = ['First Year', 'Second Year', 'Third Year', 'Fourth Year', 'Fifth Year', 'Sixth Year'];
-        // foreach($years as $year){
-        //     Year::create([
-        //         'year' => $year,
-        //     ]);
-        // }
+        $years = ['First Year', 'Second Year', 'Third Year', 'Fourth Year', 'Fifth Year', 'Sixth Year'];
+        foreach($years as $year){
+            Year::create([
+                'year' => $year,
+            ]);
+        }
     }
 }
